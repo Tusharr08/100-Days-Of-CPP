ff9c7a1c554ad3d346ce01fead2433251d0cc107
Tushar Gupta <guptatushar080202@gmail.com>
2021-10-02 14:47:50 +0530

    maps unordered_maps multimaps unordered_multimaps

---
+++ Day 21/multimap.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Multimap is similar to map with an addition that multiple elements can have same keys.
+ Also, it is NOT required that the key value and mapped value pair has to be unique in this case.
+  One important thing to note about multimap is that multimap keeps all the keys in sorted order always.
+   These properties of multimap makes it very much useful in competitive programming.
+*/
+
+int main()
+{
+    multimap<int,int> gquiz1;
+    gquiz1.insert(pair <int, int> (1, 40));
+    gquiz1.insert(pair <int, int> (2, 30));
+    gquiz1.insert(pair <int, int> (3, 60));
+    gquiz1.insert(pair <int, int> (6, 50));
+    gquiz1.insert(pair <int, int> (6, 10));
+
+    multimap<int,int> :: iterator itr;
+    cout<<"gquiz:"<<endl;
+    for(itr=gquiz1.begin();itr!=gquiz1.end();itr++)
+    {
+        cout<<itr->first<<"-->"<<itr->second<<endl;
+    }
+    gquiz1.insert(pair <int,int> (4,50));
+
+    cout<<"gquiz after inserting :"<<endl;
+    for(itr=gquiz1.begin();itr!=gquiz1.end();itr++)
+    {
+        cout<<itr->first<<"-->"<<itr->second<<endl;
+    }
+}
+
---
+++ Day 21/unordered_map.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_map<int,int> order;
+    order[5] = 10;
+    order[3] = 5;
+    order[20] = 100;
+    order[1] = 1;
+
+    for(auto i=order.begin();i!=order.end();i++)
+    {
+        cout<<i->first<<"-->"<<i->second<<endl;
+    }
+
+}
