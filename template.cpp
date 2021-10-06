7fc1cc1cc3239bad8694c6565ba997b3c49bf988
Tushar Gupta <guptatushar080202@gmail.com>
2021-10-07 02:50:39 +0530

    added more defined macros

--- template.cpp
+++ template.cpp
@@ -1,12 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define tc ll t sc cin >>t sc while(t--)
+//#include<ext/pb_ds/assoc_container.hpp>
+//using namespace std;
+//using namespace __gnu_pbds;
+
 #define ff first
-#define sc ;
 #define ss second
-#define pp push_back
+#define tc ll t sc cin >>t sc while(t--)
+#define sc ;
+#define pb push_back
 #define pp pop_back
 #define mp make_pair
-#define ll long long
+#define int long long
+#define mt make_tuple
+#define pii pair<int,int>
+#define vi vector<int>
+#define mii map<int,int>
+#define pqb priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define mod 1000000007
+#define inf 1e18
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define mk(arr,n,type) type *arr=new type[n];
+#define range(a,b) substr(a,b-a+1)
+#define trace(x) cerr<<#x<<": "<<<<x<<" "<<endl
 #define radhe ios::sync_with_stdio(false);
 #define krishna cin.tie(NULL);
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
